130c908a8aa873adbb84d77fa827e25c6e2f30d4
Mahidbelim <96978859+Mahidbelim@users.noreply.github.com>
2026-09-20 13:13:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "success",
+        "message": "Mahid Stock Scanner backend is running"
+    })
+
+@app.route("/api/health")
+def health():
+    return jsonify({
+        "status": "ok"
+    })
+
+if __name__ == "__main__":
+    app.run()
